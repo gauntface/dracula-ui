3041c22cf308f141dac4c287a53cde6d2ef79b4d
Matt Gaunt-Seo <matt@gaunt.dev>
2022-11-05 11:42:02 -0700

    More shifting of stuff to new structure and remove JS and website

--- examples/with-react/src/App.js
+++
@@ -1,8 +0,0 @@
-import 'dracula-ui/styles/dracula-ui.css'
-import { Paragraph } from 'dracula-ui'
-
-function App() {
-  return <Paragraph color="black">Hello Vampire</Paragraph>
-}
-
-export default App;
