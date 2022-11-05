6a190e53c6f760effa281257caa72e5f64df912d
Matt Gaunt-Seo <matt@gaunt.dev>
2022-11-05 16:34:41 -0700

    Allow state

--- src/styles-gen/color-variants.js
+++ src/styles-gen/color-variants.js
@@ -5,7 +5,7 @@ import {ALL_COLORS} from './config/colors.js';
 async function generateCSS(outdir, name, color, cssfn, opts) {
   const selector = `drac-${name}-${color}${opts.suffix || ''}`
   const rules = cssfn(color);
-  const css = `.${selector} ${opts.selector || ''} {
+  const css = `.${selector}${opts.state || ''} ${opts.selector || ''} {
   ${rules}
 }
 `;
